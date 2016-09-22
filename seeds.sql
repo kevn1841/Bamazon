ec393c04c1a9ceedf90900befe4bd8d7c8daedef
Kevin Shukla <kevn1841@gmail.com>
2016-09-22 17:22:54 -0400

    seeds.sql filled in

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO forsale(product, departmentField, price, quantity)VALUES("Chandelier","Furniture",59.99,5),("55' Smart TV","Electronics",179.99,20),("55' TV Wall Mount","Accessories",49.95,3),("Royal Dining Table","Furniture",250.00,1),("HP Laptop","Electronics",0.99 ,99),("Batman v Superman poster","Accessories",5.99 ,12),("Autographed Lebron James Basketball","Item of extreme value",189.99 ,1),("Belgium Assorted Chocolates","Food/Drink",8.99 ,30),("Dunkin Donuts Premium Roast","Food/Drink",10.99 ,23),(" 1957 Chevy Bel Air","Car",35,000.00,1)
+
+
+
+INSERT INTO products(ProductName,DepartmentName,Price,StockQuantity)VALUES ("Uncharted 4","Video Games",49.95,150),
+    ("DOOM","Video Games",59.99,200),
+    ("Crate of Spam","Food and Drink",24.50,50),
+    ("Cool Shades","Apparel",75.00,5),
+    ("Worn Denim Jeans","Apparel",54.25,35),
+    ("Survival Towel","Necessities",42.42,42),
+    ("Bill and Ted's Excellent Adventure","Films",15.00,25),
+    ("Mad Max: Fury Road","Films",25.50,57),
+    ("Monopoly","Board Games",30.50,35),
+    ("Yahtzee","Board Games",19.95,23)
