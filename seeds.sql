a19bebb64b5b81617bce3ccfc22987a09a369a7f
Kevin Shukla <kevn1841@gmail.com>
2016-09-22 17:58:11 -0400

    seeds.sql error fixed

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,10 @@
-INSERT INTO forsale(product, departmentField, price, quantity)VALUES("Chandelier","Furniture",59.99,5),("55' Smart TV","Electronics",179.99,20),("55' TV Wall Mount","Accessories",49.95,3),("Royal Dining Table","Furniture",250.00,1),("HP Laptop","Electronics",0.99 ,99),("Batman v Superman poster","Accessories",5.99 ,12),("Autographed Lebron James Basketball","Item of extreme value",189.99 ,1),("Belgium Assorted Chocolates","Food/Drink",8.99 ,30),("Dunkin Donuts Premium Roast","Food/Drink",10.99 ,23),(" 1957 Chevy Bel Air","Car",35,000.00,1)
-
-
-
-INSERT INTO products(ProductName,DepartmentName,Price,StockQuantity)VALUES ("Uncharted 4","Video Games",49.95,150),
-    ("DOOM","Video Games",59.99,200),
-    ("Crate of Spam","Food and Drink",24.50,50),
-    ("Cool Shades","Apparel",75.00,5),
-    ("Worn Denim Jeans","Apparel",54.25,35),
-    ("Survival Towel","Necessities",42.42,42),
-    ("Bill and Ted's Excellent Adventure","Films",15.00,25),
-    ("Mad Max: Fury Road","Films",25.50,57),
-    ("Monopoly","Board Games",30.50,35),
-    ("Yahtzee","Board Games",19.95,23)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Chandelier","Furniture",59.99,5)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("55' Smart TV","Electronics",179.99,20)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("55' TV Wall Mount","Accessories",49.95,3)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Royal Dining Table","Furniture",250.00,1)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("HP Laptop","Electronics",0.99 ,99)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Batman v Superman poster","Accessories",5.99 ,12)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Autographed Lebron James Basketball","Item of extreme value",189.99 ,1)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Belgium Assorted Chocolates","Food/Drink",8.99 ,30)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Dunkin Donuts Premium Roast","Food/Drink",10.99 ,23)
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Universal Laptop Charger","Electronics",70,1)
--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var connection=mysql.createConnection({
 connection.connect(function(err){
 	if(err){
 		console.error("error connecting: "+ err.stack);}
+		createListing();
+
+
+
+
+		function createListing(){
 	connection.query('SELECT * FROM products',function(err,res){
 		if(err)throw err;
 		//PRINTS THE TABLE TO THE CONSOLE WITH MINIMAL STYLING//
@@ -24,3 +30,43 @@ connection.connect(function(err){
 		//RUNS THE CUSTOMER'S PROMPTS AFTER CREATING THE TABLE. SENDS res SO THE promptCustomer FUNCTION IS ABLE TO SEARCH THROUGH THE DATA//
 		promptUser(res);})}})
 
+var promptCustomer=function(res){
+	//PROMPTS USER FOR WHAT THEY WOULD LIKE TO PURCHASE//
+	inquirer.prompt([
+		{type:'input',
+		name:'choice',
+		message:'What would you like to purchase? [Quit with Q]'}]).then(function(val){
+		//SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
+		var correct=false;
+		//LOOPS THROUGH THE MYSQL TABLE TO CHECK THAT THE PRODUCT THEY WANTED EXISTS//
+		for(var i=0;i<res.length;i++){
+			//IF THE PRODUCT EXISTS, SAVE THE DATA FOR SAID PRODUCT WITHIN THE product AND id VARIABLES//
+			if(res[i].product==val.choice){
+				var correct=true;
+				var Product=val.choice;
+				var id=i;
+				//PROMPTS THE USER TO SEE HOW MANY OF THE PRODUCT THEY WOULD LIKE TO BUY//
+				inquirer.prompt([
+					{type:'input',
+					name:'quant',
+					message:"How many would you like to buy?"}]).then(function(val){
+					//CHECKS TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE//
+					if((res[id].quantity-val.quant)>0){
+						//REMOVES THE AMOUNT REQUESTED FROM THE MYSQL TABLE//
+						connection.query("UPDATE forsale SET quantity='"+(res[id].quantity-val.quant)+"' WHERE product='"+Product+"'", function(err, res2){
+							if(err)throw err;
+							//TELLS THE USER THAT THE PRODUCT HAS BEEN PURCHASED//
+							console.log("PRODUCT BOUGHT!");
+							//REWRITES THE TABLE AND STARTS AGAIN//
+							createListing();})}
+					//IF THE AMOUNT REQUESTED WAS GREATER THAN THE AMOUNT AVAILABLE, RESTARTS PROMPTS//
+					else{
+						console.log("NOT A VALID SELECTION!");
+						promptCustomer(res);}})}
+			//IF THE USER INPUTTED Q, EXIT PROGRAM//
+			if(val.choice=="Q"||val.choice=="q"){process.exit()}}
+		//IF THE PRODUCT REQUESTED DOES NOT EXIST, RESTARTS PROMPTS//
+		if(i==res.length&&correct==false){
+			console.log("NOT A VALID SELECTION");
+			promptCustomer(res);}
+		})}
