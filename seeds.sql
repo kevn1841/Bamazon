6cfadfec5e640d2580cdd6b8dd3bcbbe4928c30d
Kevin Shukla <kevn1841@gmail.com>
2016-09-22 18:33:24 -0400

    finished product

--- seeds.sql
+++ seeds.sql
@@ -8,3 +8,8 @@ INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Autographed L
 INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Belgium Assorted Chocolates","Food/Drink",8.99 ,30)
 INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Dunkin Donuts Premium Roast","Food/Drink",10.99 ,23)
 INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Universal Laptop Charger","Electronics",70,1)
+
+
+//or
+
+INSERT INTO forsale(product,departmentField,price,quantity)VALUES("Chandelier","Furniture",59.99,5),("55' Smart TV","Electronics",179.99,20),("55' TV Wall Mount","Accessories",49.95,3),("Royal Dining Table","Furniture",250.00,1),("HP Laptop","Electronics",0.99 ,99),("Batman v Superman poster","Accessories",5.99 ,12),("Autographed Lebron James Basketball","Item of extreme value",189.99 ,1),("Belgium Assorted Chocolates","Food/Drink",8.99 ,30),("Dunkin Donuts Premium Roast","Food/Drink",10.99 ,23),("Universal Laptop Charger","Electronics",70,1)
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var inquirer=require('inquirer');
 var connection=mysql.createConnection({
 		host:"localhost",
 		port: 3306,
-		user:"root", //Your username//
-		password:"asdfghjkl8465", //Your password//
+		user:"root",
+		password:"asdfghjkl8465",
 		database:"bamazon"})
 
 connection.connect(function(err){
@@ -13,60 +13,52 @@ connection.connect(function(err){
 		console.error("error connecting: "+ err.stack);}
 		createListing();
 
+})
 
-
-
-		function createListing(){
-	connection.query('SELECT * FROM products',function(err,res){
+	function createListing(){
+	connection.query('SELECT * FROM forsale',function(err,res){
 		if(err)throw err;
-		//PRINTS THE TABLE TO THE CONSOLE WITH MINIMAL STYLING//
 		var tab="\t";
 		console.log("id\tproduct\tdepartmentField\tprice\tquantity");
-		console.log("--------------------------------------------------------");
-		//FOR LOOP GOES THROUGH THE MYSQL TABLE AND PRINTS EACH INDIVIDUAL ROW ON A NEW LINE//
+		console.log("========================================================");
 		for(var i=0;i<res.length;i++){
-			console.log(res[i].id+tab+res[i].product+tab+res[i].departmentField+tab+res[i].price+tab+res[i].quantity);}
-		console.log("--------------------------------------------------------");
-		//RUNS THE CUSTOMER'S PROMPTS AFTER CREATING THE TABLE. SENDS res SO THE promptCustomer FUNCTION IS ABLE TO SEARCH THROUGH THE DATA//
-		promptUser(res);})}})
+			console.log(res[i].id+tab+res[i].product+tab+res[i].departmentField+tab+res[i].price+tab+res[i].quantity+'\n'+'\n');}
+		console.log("========================================================");
+		promptCustomer(res);})}
 
-var promptCustomer=function(res){
-	//PROMPTS USER FOR WHAT THEY WOULD LIKE TO PURCHASE//
+function promptCustomer(res){
 	inquirer.prompt([
 		{type:'input',
 		name:'choice',
-		message:'What would you like to purchase? [Quit with Q]'}]).then(function(val){
-		//SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
+		message:'Type in the item(s) that you wish to purchase. Type Q to quit.'}]).then(function(val){
 		var correct=false;
-		//LOOPS THROUGH THE MYSQL TABLE TO CHECK THAT THE PRODUCT THEY WANTED EXISTS//
 		for(var i=0;i<res.length;i++){
-			//IF THE PRODUCT EXISTS, SAVE THE DATA FOR SAID PRODUCT WITHIN THE product AND id VARIABLES//
 			if(res[i].product==val.choice){
 				var correct=true;
 				var Product=val.choice;
 				var id=i;
-				//PROMPTS THE USER TO SEE HOW MANY OF THE PRODUCT THEY WOULD LIKE TO BUY//
 				inquirer.prompt([
 					{type:'input',
 					name:'quant',
-					message:"How many would you like to buy?"}]).then(function(val){
-					//CHECKS TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE//
+					message:"Type in the quantity"}]).then(function(val){
 					if((res[id].quantity-val.quant)>0){
-						//REMOVES THE AMOUNT REQUESTED FROM THE MYSQL TABLE//
 						connection.query("UPDATE forsale SET quantity='"+(res[id].quantity-val.quant)+"' WHERE product='"+Product+"'", function(err, res2){
 							if(err)throw err;
-							//TELLS THE USER THAT THE PRODUCT HAS BEEN PURCHASED//
-							console.log("PRODUCT BOUGHT!");
-							//REWRITES THE TABLE AND STARTS AGAIN//
-							createListing();})}
-					//IF THE AMOUNT REQUESTED WAS GREATER THAN THE AMOUNT AVAILABLE, RESTARTS PROMPTS//
+							createListing();
+							thankYou();
+						}
+
+							)}
+
 					else{
-						console.log("NOT A VALID SELECTION!");
+						console.log("Please put in a proper request");
 						promptCustomer(res);}})}
-			//IF THE USER INPUTTED Q, EXIT PROGRAM//
-			if(val.choice=="Q"||val.choice=="q"){process.exit()}}
-		//IF THE PRODUCT REQUESTED DOES NOT EXIST, RESTARTS PROMPTS//
+			if(val.choice=="Q"){process.exit()}}
 		if(i==res.length&&correct==false){
-			console.log("NOT A VALID SELECTION");
+			console.log("Please put in a proper request");
 			promptCustomer(res);}
 		})}
+
+		function thankYou(){
+			console.log("Enjoy your new product(s)")
+		};
